a91407203a949bf6c6a090b74bcc9b0d617895fe
Adamkgould <Adamkengould@gmail.com>
2026-02-28 21:10:35 -0500

    fix: clean up route.ts, update env to new Supabase

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -17,10 +17,3 @@ export async function GET(request: Request) {
 
   return NextResponse.redirect(`${origin}/login?error=auth_callback_failed`);
 }
-```
-
-Last file! Now create your **`.env.local`** in the project root (next to `package.json`) with this:
-```
-NEXT_PUBLIC_SUPABASE_URL=https://bnmtynevbuplqpuqvmna.supabase.co
-NEXT_PUBLIC_SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJubXR5bmV2YnVwbHFwdXF2bW5hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzMDMyMzMsImV4cCI6MjA4Nzg3OTIzM30.JUzPTI9WnDHp6Jrab4jMrCjhHqNrjGZEDDf5Ae7xOg8
-NEXT_PUBLIC_SITE_URL=https://704collective.com
